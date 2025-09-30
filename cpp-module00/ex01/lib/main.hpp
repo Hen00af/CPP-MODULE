2f69f0bbfaa012e4e6558a6dcffb60e146db435d
Seiya Hattori <shattori@c4r2s12.42tokyo.jp>
2025-10-01 00:09:32 +0900

    tno

---
+++ cpp-module00/ex01/lib/main.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shattori <shattori@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 03:29:38 by shattori          #+#    #+#             */
+/*   Updated: 2025/09/24 15:52:21 by shattori         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cctype>
+#include "../lib/Contact.hpp"
+#include "../lib/PhoneBook.hpp"
+
+#endif
