4f9295836525076c138ff0aa231fbcfb0a77a449
Seiya Hattori <shattori@c6r3s7.42tokyo.jp>
2025-11-21 20:03:57 +0900

    ex02isdone

--- cpp-module04/ex01/main.cpp
+++ cpp-module04/ex01/main.cpp
@@ -9,15 +9,19 @@ int main()
     const int N = 10;
     Animal* animals[N];
 
+    std::cout << "\n====Dog constructor called====\n" << std::endl;
     for (int i = 0; i < N / 2; i++)
         animals[i] = new Dog();
 
+    std::cout << "\n====Cat constructor called====\n" << std::endl;
     for (int i = N / 2; i < N; i++)
         animals[i] = new Cat();
 
+    std::cout << "\n====Destructor called====\n" << std::endl;
     for (int i = 0; i < N; i++)
         delete animals[i];
 
+    std::cout << "\n====Deep copy test====\n" << std::endl;
     Dog basic;
     {
         Dog tmp = basic;
