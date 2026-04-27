41823b1c8eb9c94fbca7a568972da947945340a3
Hen00af <hunny_h_1123@icloud.com>
2026-04-27 18:47:37 +0900

    ex00

--- cpp-module09/ex00/BitcoinExchange.cpp
+++ cpp-module09/ex00/BitcoinExchange.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <sstream>
 
 BitcoinExchange::BitcoinExchange() {}
 
@@ -11,7 +12,10 @@ BitcoinExchange::BitcoinExchange(std::string const &dbFile)
 		throw std::runtime_error("Error: could not open database file.");
 }
 
-BitcoinExchange::BitcoinExchange(BitcoinExchange const &copy) : _database(copy._database) {}
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &copy)
+	: _database(copy._database)
+{
+}
 
 BitcoinExchange::~BitcoinExchange() {}
 
@@ -22,53 +26,99 @@ BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs)
 	return *this;
 }
 
-bool BitcoinExchange::isValidDate(std::string const &date) const
+bool operator<(Date const &a, Date const &b)
 {
-	if (date.length() != 10 || date[4] != '-' || date[7] != '-')
+	if (a.y != b.y)
+		return a.y < b.y;
+	if (a.m != b.m)
+		return a.m < b.m;
+	return a.d < b.d;
+}
+
+bool BitcoinExchange::parseDate(std::string const &str, Date &date) const
+{
+	if (str.length() != 10 || str[4] != '-' || str[7] != '-')
 		return false;
+
 	for (int i = 0; i < 10; i++)
 	{
 		if (i == 4 || i == 7)
 			continue;
-		if (date[i] < '0' || date[i] > '9')
+		if (str[i] < '0' || str[i] > '9')
 			return false;
 	}
-	int year = std::atoi(date.substr(0, 4).c_str());
-	int month = std::atoi(date.substr(5, 2).c_str());
-	int day = std::atoi(date.substr(8, 2).c_str());
-	if (year < 1 || month < 1 || month > 12 || day < 1 || day > 31)
+
+	date.y = std::atoi(str.substr(0, 4).c_str());
+	date.m = std::atoi(str.substr(5, 2).c_str());
+	date.d = std::atoi(str.substr(8, 2).c_str());
+
+	if (date.y < 1 || date.m < 1 || date.m > 12)
 		return false;
-	if (month == 2)
+	if (date.d < 1 || date.d > 31)
+		return false;
+
+	if (date.m == 2)
 	{
-		bool leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-		if (day > (leap ? 29 : 28))
+		bool leap;
+
+		leap = (date.y % 4 == 0 && date.y % 100 != 0)
+			|| (date.y % 400 == 0);
+		if (date.d > (leap ? 29 : 28))
 			return false;
 	}
-	else if (month == 4 || month == 6 || month == 9 || month == 11)
+	else if (date.m == 4 || date.m == 6 || date.m == 9 || date.m == 11)
 	{
-		if (day > 30)
+		if (date.d > 30)
 			return false;
 	}
+
 	return true;
+}
+
+std::string BitcoinExchange::dateToString(Date const &date) const
+{
+	std::ostringstream oss;
+
+	oss << date.y << "-";
+	if (date.m < 10)
+		oss << "0";
+	oss << date.m << "-";
+	if (date.d < 10)
+		oss << "0";
+	oss << date.d;
+
+	return oss.str();
 }
 
 bool BitcoinExchange::loadDatabase(std::string const &filename)
 {
 	std::ifstream file(filename.c_str());
+
 	if (!file.is_open())
 		return false;
 
 	std::string line;
-	std::getline(file, line); // skip header
+	std::getline(file, line);
+
 	while (std::getline(file, line))
 	{
-		std::string::size_type pos = line.find(',');
+		std::string::size_type pos;
+		std::string dateStr;
+		Date date;
+		double rate;
+
+		pos = line.find(',');
 		if (pos == std::string::npos)
 			continue;
-		std::string date = line.substr(0, pos);
-		double rate = std::atof(line.substr(pos + 1).c_str());
+
+		dateStr = line.substr(0, pos);
+		if (!parseDate(dateStr, date))
+			continue;
+
+		rate = std::atof(line.substr(pos + 1).c_str());
 		_database[date] = rate;
 	}
+
 	file.close();
 	return true;
 }
@@ -76,6 +126,7 @@ bool BitcoinExchange::loadDatabase(std::string const &filename)
 void BitcoinExchange::processInput(std::string const &inputFile) const
 {
 	std::ifstream file(inputFile.c_str());
+
 	if (!file.is_open())
 	{
 		std::cerr << "Error: could not open file." << std::endl;
@@ -83,53 +134,70 @@ void BitcoinExchange::processInput(std::string const &inputFile) const
 	}
 
 	std::string line;
-	std::getline(file, line); // skip header
+	std::getline(file, line);
+
 	while (std::getline(file, line))
 	{
-		std::string::size_type sep = line.find(" | ");
+		std::string::size_type sep;
+		std::string dateStr;
+		std::string valueStr;
+		Date date;
+		char *endptr;
+		double value;
+
+		sep = line.find(" | ");
 		if (sep == std::string::npos)
 		{
 			std::cerr << "Error: bad input => " << line << std::endl;
 			continue;
 		}
-		std::string date = line.substr(0, sep);
-		std::string valueStr = line.substr(sep + 3);
 
-		if (!isValidDate(date))
+		dateStr = line.substr(0, sep);
+		valueStr = line.substr(sep + 3);
+
+		if (!parseDate(dateStr, date))
 		{
-			std::cerr << "Error: bad input => " << date << std::endl;
+			std::cerr << "Error: bad input => " << dateStr << std::endl;
 			continue;
 		}
 
-		char *endptr;
-		double value = std::strtod(valueStr.c_str(), &endptr);
+		value = std::strtod(valueStr.c_str(), &endptr);
 		if (*endptr != '\0' && *endptr != '\r')
 		{
 			std::cerr << "Error: bad input => " << line << std::endl;
 			continue;
 		}
+
 		if (value < 0)
 		{
 			std::cerr << "Error: not a positive number." << std::endl;
 			continue;
 		}
+
 		if (value > 1000)
 		{
 			std::cerr << "Error: too large a number." << std::endl;
 			continue;
 		}
 
-		std::map<std::string, double>::const_iterator it = _database.lower_bound(date);
-		if (it == _database.end() || it->first != date)
+		std::map<Date, double>::const_iterator it;
+		it = _database.lower_bound(date);
+
+		if (it == _database.end() || it->first < date || date < it->first)
 		{
 			if (it == _database.begin())
 			{
-				std::cerr << "Error: bad input => " << date << std::endl;
+				std::cerr << "Error: bad input => " << dateStr << std::endl;
 				continue;
 			}
 			--it;
 		}
-		std::cout << date << " => " << value << " = " << (value * it->second) << std::endl;
+
+		std::cout << dateToString(date)
+				  << " => " << value
+				  << " = " << value * it->second
+				  << std::endl;
 	}
+
 	file.close();
 }
--- cpp-module09/ex00/BitcoinExchange.hpp
+++ cpp-module09/ex00/BitcoinExchange.hpp
@@ -1,25 +1,35 @@
 #ifndef BITCOINEXCHANGE_HPP
-#define BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
 
-#include <string>
-#include <map>
+# include <map>
+# include <string>
+
+struct Date
+{
+	int y;
+	int m;
+	int d;
+};
+
+bool operator<(Date const &a, Date const &b);
 
 class BitcoinExchange
 {
 private:
-	std::map<std::string, double>	_database;
+	std::map<Date, double> _database;
 
-	BitcoinExchange();
-
-	bool	isValidDate(std::string const &date) const;
-	bool	loadDatabase(std::string const &filename);
+	bool		parseDate(std::string const &str, Date &date) const;
+	std::string	dateToString(Date const &date) const;
 
 public:
+	BitcoinExchange();
 	BitcoinExchange(std::string const &dbFile);
 	BitcoinExchange(BitcoinExchange const &copy);
 	~BitcoinExchange();
+
 	BitcoinExchange &operator=(BitcoinExchange const &rhs);
 
+	bool	loadDatabase(std::string const &filename);
 	void	processInput(std::string const &inputFile) const;
 };
 
